1fec06b33f279bcd70e6e8a4702780854f19606b
JGwanghou <jogwangho0619@gmail.com>
2023-01-02 17:48:36 +0900

    @

---
+++ Ch01/hello.py
@@ -0,0 +1,10 @@
+"""
+    날짜 : 2023 / 01 / 02
+    이름 : 조광호
+    내용 : 파이썬 hello world 실습
+"""
+
+print("Hello world!")
+
+print('Hello Python!')
+print('------')
---
+++ Ch02/2.자료형.py
@@ -0,0 +1,50 @@
+"""
+    날짜: 2023/01/02
+    이름: 조광호
+    내용: 파이썬 자료형 실습하기
+"""
+
+# 정수형
+var1 = 1
+var2 = 2
+var3 = 3
+
+print('var1 : ', var1)
+print('var2 : ', var2)
+print('var3 : ', var3)
+print('var1 Type : ', type(var1))
+print('var2 Type : ', type(var2))
+print('var3 Type : ', type(var3))
+
+# 실수형
+var4 = 0.4
+var5 = 1.123456789
+
+print('var4 : ', var4)
+print('var5 : ', var5)
+print('var4 : ', type(var4))
+print('var5 : ', type(var5))
+
+# 논리형
+var6 = True
+var7 = False
+
+print('var6 : ', var6)
+print('var7 : ', var7)
+print('var6 type : ', type(var6))
+print('var7 type : ', type(var7))
+
+# 문자열
+str1 = 'A'
+str2 = 'Apple'
+str3 = "Apple"
+str4 = "사과"
+
+print("str1 : ", str1)
+print("str2 : ", str2)
+print("str3 : ", str3)
+print("str4 : ", str4)
+print("str1 Type : ", type(str1))
+print("str2 Type : ", type(str2))
+print("str3 Type : ", type(str3))
+print("str4 Type : ", type(str4))
